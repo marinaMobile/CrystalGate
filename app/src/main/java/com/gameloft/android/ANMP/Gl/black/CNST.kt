9997283b39a23b3f21c8cc6308ee7a131600d7a9
Marina <marina@Marinas-MacBook-Pro.local>
2022-07-14 11:53:28 +0300

    first commit

---
+++ app/src/main/java/com/gameloft/android/ANMP/Gl/black/CNST.kt
@@ -0,0 +1,16 @@
+package com.gameloft.android.ANMP.Gl.black
+
+object CNST {
+    const val AF_DEV_KEY = "MhRfeGCw8K6VAMCLJDh8ND"
+    const val jsoupCheck = "6v1z"
+    const val ONESIGNAL_APP_ID = "1945c68f-79b3-4785-9be1-075316aa06ff"
+
+    var lru = "http://crystalgate.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
---
+++ app/src/main/java/com/gameloft/android/ANMP/Gl/white/Game.kt
@@ -0,0 +1,13 @@
+package com.gameloft.android.ANMP.Gl.white
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gameloft.android.ANMP.Gl.R
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+    }
+}
